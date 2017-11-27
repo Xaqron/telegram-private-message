613cb5d916455cceb73fddbdb4331f972aa8179d
masoud <mosibatzadeh@yahoo.com>
2017-11-27 19:15:58 +0330

    minimized to less than 30 lines

--- tpm.js
+++ tpm.js
@@ -22,11 +22,7 @@ bot.on('message', async (msg) => {
       await bot.sendMessage(id, `Sender: ${msg.chat.id}\n\n${lines.join('\n')}`, {parse_mode: 'HTML'})
       await bot.sendMessage(msg.chat.id, 'Your message sent')
     } catch (err) {
-      if (err.message.indexOf('chat not found') > 0) {
-        bot.sendMessage(msg.chat.id, `User has not joined yet.`)
-      } else {
-        bot.sendMessage(msg.chat.id, `Only text and emoji are allowed.`)
-      }
+      bot.sendMessage(msg.chat.id, `User has not joined yet.`)
     }
   }
 })
